c996db8c3c64b901139a9cb67c60971b2aa27d56
lululinnea0-crypto <lululinnea0@gmail.com>
2026-07-17 17:09:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -59,3 +59,65 @@ document.querySelectorAll(".colores button").forEach(boton => {
     });
 
 });
+
+// ==========================
+// AGREGAR PRODUCTO AL CARRITO
+// ==========================
+
+const botonAgregar = document.querySelector(".btn-agregar");
+
+if (botonAgregar) {
+
+    botonAgregar.addEventListener("click", () => {
+
+
+        if (talleSeleccionado === "") {
+
+            alert("Elegí un talle");
+            return;
+
+        }
+
+
+        if (colorSeleccionado === "") {
+
+            alert("Elegí un color");
+            return;
+
+        }
+
+
+        const producto = {
+
+            nombre: document.querySelector(".info-producto h1").textContent,
+
+            precio: 68000,
+
+            imagen: document.querySelector(".galeria img").src,
+
+            talle: talleSeleccionado,
+
+            color: colorSeleccionado,
+
+            cantidad: 1,
+
+            pago: "50%"
+
+        };
+
+
+        let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+
+        carrito.push(producto);
+
+
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+
+
+        alert("🛒 Producto agregado al carrito");
+
+
+    });
+
+}
