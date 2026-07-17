d897b59d0ba3e7f01d2e98330931ee342f4d3795
lululinnea0-crypto <lululinnea0@gmail.com>
2026-07-17 16:48:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// ==========================
+// CARRITO L&L BOUTIQUE
+// ==========================
 
+// Obtener carrito guardado
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+// Guardar carrito
+function guardarCarrito() {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+// Agregar producto
+function agregarAlCarrito(producto) {
+    carrito.push(producto);
+    guardarCarrito();
+
+    alert("🛍️ Producto agregado al carrito.");
+}
